83edd713a9980c496d6c032b318e938ea4bdb407
Seven <seven_jang@yahoo.ca>
2020-05-02 23:21:22 -0400

    Finished score text
    can change masks, sanitzier and score values and automatically updates itself constatnly

--- 6-ft-apart/ScoreText.java
+++ 6-ft-apart/ScoreText.java
@@ -12,19 +12,52 @@ public class ScoreText extends Actor
      * Act - do whatever the ScoreText wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
-    public static int score;
-    
+    GreenfootImage maskImg = new GreenfootImage((new FaceMask()).getImage());
+    GreenfootImage sanitizerImg = new GreenfootImage((new Sanitizer()).getImage());
+    public static int score = 0;
+    public static int masks = 3;
+    public static int sanitizer = 3;
+    GreenfootImage scoreBoard = new GreenfootImage(175,20);
+    GreenfootImage scoreImg = new GreenfootImage("Score: "+score+" M x"+masks+" S x"+sanitizer, 20, Color.BLACK,new Color(213,172, 11));
+    public ScoreText(){
+        scoreBoard.setColor(new Color(213,172, 11));
+        score =0;
+        masks = 3;
+        sanitizer = 3;
+        scoreBoard.fill();
+        setImage(scoreBoard);
+    }
+    public int getMasks(){
+        return masks;
+    }
+    public int getSanitizer(){
+        return sanitizer;
+    }
+    public void addMask(int num){
+        masks+=num;
+    }
+    public void addSanitizer(int num){
+        sanitizer+=num;
+    }
     public static int getScore(){
         return score;
     }
-    
-    public ScoreText(){
-        
+    public void setScore(int score){
+        this.score = score;
     }
-    
+    public void addScore(int num){
+        score+=num;
+    }
     public void act() 
     {
-        // Add your action code here.
+        //constantly updates the score and masks and sanitizers
+        scoreBoard.fill();
+        scoreImg = new GreenfootImage("Score: "+score, 20, Color.BLACK,new Color(213,172, 11));
+        scoreBoard.drawImage(scoreImg,0,0);
+        scoreBoard.drawImage(maskImg,scoreImg.getWidth(),0);
+        scoreBoard.drawImage(new GreenfootImage(" x"+masks, 20, Color.BLACK,new Color(213,172, 11)),scoreImg.getWidth()+maskImg.getWidth(),0);
+        scoreBoard.drawImage(sanitizerImg,scoreImg.getWidth()+maskImg.getWidth()+30,0);
+        scoreBoard.drawImage(new GreenfootImage(" x"+sanitizer, 20, Color.BLACK,new Color(213,172, 11)),scoreImg.getWidth()+maskImg.getWidth()+30+sanitizerImg.getWidth(),0);
+        setImage(scoreBoard);
     }    
 }
